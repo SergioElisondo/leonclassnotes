6d1519f3ff4c02224e0970e6079ba38e8e373669
Sergio Elisondo <sergioelisondo@gmail.com>
2021-03-15 11:33:00 -0700

    this is a simple filter method in use

---
+++ simple filter method.js
@@ -0,0 +1,5 @@
+// Please create a function that takes in an array of numbers and returns a new array that only contains odd numbers. Review our last practice problem from last class if you need some ideas.
+
+const num1 = [1, 2, 3, 4, 5, 6, 7]
+const newArr = num1.filter(a => a % 3 === 0)
+console.log(newArr)
