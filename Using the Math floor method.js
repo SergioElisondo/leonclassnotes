d7f3e0cb18d5a31d3b9cb1b8d42926a80fae4e4e
Sergio Elisondo <sergioelisondo@gmail.com>
2021-03-16 00:43:40 -0700

    this is a simple example of Using the Math.floor() method

---
+++ Using the Math floor method.js
@@ -0,0 +1,7 @@
+// Please create a function that takes in two numbers. Print to the console the division of the these two numbers rounded down. 
+
+function divideWithFloor(num1, num2){
+    let divideTwoNumbers = num1 / num2
+    console.log(Math.floor(divideTwoNumbers))
+}
+divideWithFloor(124, 30)
